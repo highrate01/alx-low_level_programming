823b23b4d525cffacb01304fd20292d4295e4950
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-07 15:09:33 -0700

    first c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * C programming
+ * main - This printout the output using puts
+ *
+ * Return: always(0)
+ *
  */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
