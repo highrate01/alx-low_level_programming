cbbf1b907815f2199cb25da6d8d07fc1bc6a7cf9
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-07 15:23:18 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This program print the outcome of a piece of art using printf
+ *
+ * Return: always 0(success)
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
