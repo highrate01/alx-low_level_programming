ee61b3d4288a4187c2ac36b37e95dfc1723388cb
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-09 11:20:40 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ *
+ * Return: always 0(success)
+ *
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a < 10; a++)
+	printf("%d", a);
+		putchar('\n');
+		return (0);
+}
