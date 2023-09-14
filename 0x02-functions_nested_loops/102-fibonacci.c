0af5d229e853b2118835f48aba4bb82a630d72af
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-14 14:58:44 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,18 +7,20 @@
  */
 int main(void)
 {
-	int n = 1;
-	int m = 2;
-	int sumb;
+	unsigned long int n = 1;
+	unsigned long int m = 2;
+	unsigned long int sumb;
 	int i;
 
-	printf("%d", n);
+	printf("%lu, ", n);
 	for (i = 1; i < 50; i++)
 	{
-		printf("%d", m);
+		printf("%lu", m);
 		sumb = n + m;
 		n = m;
 		m = sumb;
+		if (i != 49)
+			printf(", ");
 	}
 	printf("\n");
 return (0);
