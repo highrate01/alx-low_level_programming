92b0d48322bb68b841a1146dd7c56edbb5ed39e1
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-14 00:54:20 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - sum up two integers
+ * Description:  to add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: (0)
+ *
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
