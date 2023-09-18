ec9b3be302b102296ca242f638e5e9c07d761482
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-18 10:25:00 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value
+ * @n: The value ofdeferencing pointer
+ * Return: it reurn the nth
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
