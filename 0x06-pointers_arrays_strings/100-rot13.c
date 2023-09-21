8cc998328b53844488ac50fe2494e7a400f50821
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-21 07:21:04 -0700

    datarot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13
+ * @n: pointer to string
+ * Return: s
+ */
+char *rot13(char *n)
+{
+	int i;
+	int j;
+
+	char enc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == enc[j])
+			{
+				n[i] = rot1[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
