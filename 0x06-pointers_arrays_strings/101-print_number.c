226e908e384dc25497d85974232084a83b03fdce
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-21 07:51:55 -0700

    print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: integer to be printed
+ * Return: n
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
