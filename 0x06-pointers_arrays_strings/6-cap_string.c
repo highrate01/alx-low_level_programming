e40b5aa5209b449fe61dab67a200c2cb9acf1fcd
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-09-21 06:00:55 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,22 @@
  */
 char *cap_string(char *n)
 {
-	int index = 0;
+	int i, j;
+	char c[] = {44, 59, 46, '!', '?', '"', '(', ')', '{', '}', ' ', '\t', '\n'};
 
-	while (n[index])
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		while (!(n[index] >= 'a' && n[index] <= 'z'))
-			index++;
-
-		if  (n[index - 1] == ' ' ||
-				n[index - 1] == '\t' ||
-				n[index - 1] == '\n' ||
-				n[index - 1] == ',' ||
-				n[index - 1] == ';' ||
-				n[index - 1] == '.' ||
-				n[index - 1] == '!' ||
-				n[index - 1] == '?' ||
-				n[index - 1] == '"' ||
-				n[index - 1] == '(' ||
-				n[index - 1] == ')' ||
-				n[index - 1] == '{' ||
-				n[index - 1] == '}' ||
-				index == 0)
-			n[index] -= 32;
-		index++;
+		if (i == 0 && n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (c[j] == n[j] && (n[i + 1] >= 'a' && n[i + 1] <= 'z'))
+			{
+				n[i + 1] = n[i + 1] - 32;
+			}
+		}
 	}
 	return (n);
 }
