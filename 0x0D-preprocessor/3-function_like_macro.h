43578ae373a2c7b6539b460dfdc1ee25e4847b9c
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-10-10 12:42:11 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File - 3-function_like_macro
+ * @x: input value(int)
+ * Desc: header file that compute absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * x : (x))
+
+#endif
