ef6441605dd84ff11d49e79742c35caebf957cac
Olufemi Adebisi <olufemiadebisi35@gamail.com>
2023-10-11 11:57:42 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this program create a dog info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * description: first struct with alx
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
