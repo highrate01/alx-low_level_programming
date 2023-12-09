bcbc296776a3b61dac83f26e555b0bdfb2789f55
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2023-12-09 01:31:22 -0800

    prints all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: previoua node
+ * @next: next node
+ * Descriptioin: Doubly linked list
+ */
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *next;
+	struct dlistint_s *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+#endif
