18271ad1b7fe65510496208c0139b5e668059a31
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-16 07:32:54 -0800

    updated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,9 +9,9 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index = 0;
 	hash_node_t *bucket;
 
-	unsigned long int index = 0;
 	if (!ht || !key || !*key)
 		return (NULL);
 
