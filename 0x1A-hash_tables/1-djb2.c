9aad831479872637c37ea1f8f75a620991b7b255
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-16 03:46:16 -0800

    key -> index

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -7,7 +7,7 @@
  * Return: value of the bits
  */
 
-unsigned long int hash_djb2( const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
