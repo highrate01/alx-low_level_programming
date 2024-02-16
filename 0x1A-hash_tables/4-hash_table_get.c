619396ff5ef4414d39865c0dd52678f1a289e3e9
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-16 04:30:39 -0800

    retrieves a value associated with a key.

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: pointer to the hash table
+ * @key: key to retrieve value
+ * Return: value if key exist, null otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *bucket;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
+
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+			return (bucket->value);
+	}
+	return (NULL);
+}
