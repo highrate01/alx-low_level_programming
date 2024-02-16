90ecd363822309728855b15156767a3e562063fc
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-16 04:55:41 -0800

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ *
+ * @ht: pointer to hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *bucket;
+	int nothing = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+		while (bucket)
+		{
+			if (nothing)
+				printf(",");
+			printf("%s': '%s", bucket->key, bucket->value);
+			nothing = 1;
+			bucket = bucket->next;
+		}
+	}
+	printf("}\n");
+}
