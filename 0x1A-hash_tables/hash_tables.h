82509de329cb8c87706f0916fc11a907021c7a42
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-02-21 05:47:33 -0800

    updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,6 +32,14 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 }hash_table_t;
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -67,13 +75,7 @@ typedef struct shash_table_s
      shash_node_t *shead;
      shash_node_t *stail;
 }shash_table_t;
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2( const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
+
 void shash_table_delete(shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
@@ -81,6 +83,5 @@ shash_table_t *shash_table_create(unsigned long int size);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void sorted_list(shash_table_t *ht, shash_node_t *new_node);
-char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif /*MAIN_H*/
