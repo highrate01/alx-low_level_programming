9027de8f5a68b157886bbb628ad2e4f57d9de2f8
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-22 12:47:29 +0000

    sesrch algos

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,11 +12,11 @@ int linear_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] == value)
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (value == array[i])
 			return (i);
 	}
 	return (-1);
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: reps value to search for
+ * Return: return the index where value is located, otherwise -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t first, mid, last;
+	size_t i;
+
+	first = 0;
+	last = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (first <= last)
+	{
+		printf("Searching in array: ");
+		for (i = first; i <= last; i++)
+			printf("%d, ", array[i]);
+		printf("\n");
+
+		mid = (first + last) / 2;
+
+		if (value == array[mid])
+			return (mid);
+
+		else if (value < array[mid])
+			last = mid - 1;
+		else
+			first = mid + 1;
+	}
+	return (-1);
+}
