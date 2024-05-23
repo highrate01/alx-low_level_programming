997d7337894bd02a1b47ea27f92342d07c9a0851
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-05-23 13:48:05 +0000

    jump list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct listint_s - a singly linked list
+ * @n: reps an integer
+ * @index: index of nodes in the list
+ * @next: pointer to the next node
+ * Description: this is a singly list node
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
@@ -14,4 +29,5 @@ int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t first, size_t last, int value);
 int advanced_binary(int *array, size_t size, int value);
 int _search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
